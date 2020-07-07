aa9edd126acfe73a6946c5a9fdaef3edf9544bbe
aj9775 <37789394+aj9775@users.noreply.github.com>
2020-07-07 17:08:37 +0530

    Create index.js

---
+++ src/Store/index.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+const composerEnhancer = composeWithDevTools({
+    name: `Redux`,
+    realtime: true,
+    trace: true,
+    traceLimit: 25
+});
+
+export default createStore(composerEnhancer(applyMiddleware(ReduxThunk)));
